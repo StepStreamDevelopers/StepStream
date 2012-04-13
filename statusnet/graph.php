6b32ba0c7dba3fccabbc020bfefe9bf5463c15ba
Gayathri Premachandran <gayathri6@gatech.edu>
2012-04-13 01:23:07 -0400

    implemented graph changes

--- statusnet/graph.php
+++ statusnet/graph.php
@@ -1,37 +1,34 @@
 <?php
   include 'jpgraph.php';
   include 'jpgraph_bar.php';
-
-if (!defined('STATUSNET')) {
-    // This check helps protect against security problems;
-    // your code file can't be executed directly from the web.
-    exit(1);
-}
-
-$my_conn = mysql_connect("127.0.0.1/statusnet", "root", "celloman") or die(mysql_error());
+$my_conn = mysql_connect("statusnet", "statusnet", "PASSWORD") or die(mysql_error());
 mysql_select_db("statusnet", $my_conn ) or die(mysql_error());
 
 $profile_id = $_GET['profile_id'];
 
  $points_array = array();
     $labels_array = array();
-	$query = "SELECT * FROM weekly_points where profile_id = " . $profile_id; 
+	$query = "SELECT * FROM stepcount where profile_id = " . $profile_id; 
 		 
 	$result = mysql_query($query) or die(mysql_error());
 
 
 	while($row = mysql_fetch_array($result)){
-		 array_push($points_array,$row['cumulative_points']);
-	       array_push($labels_array,$row['week_desc']);
+		 array_push($points_array,$row['points_earned']);
+$dt = strtotime($row['step_date']); //make timestamp with datetime string
+$dt_fmt = date("m-d-y", $dt); //echo the year of the datestamp just created 
+	       array_push($labels_array,$dt_fmt);
 	}
 
 
-  $graph = new Graph(600,350);
+  $graph = new Graph(700, 400);
   $graph->SetScale('textint');
+$graph->xaxis->SetTickLabels($labels_array);
   $plot = new BarPlot($points_array );
 
-$graph->xaxis->title->Set('(Week)');
-$graph->yaxis->title->Set('(Points)');
+
+  $graph->xaxis->title->Set('(Date)');
+  $graph->yaxis->title->Set('(Points)');
   $graph->Add($plot);
   $graph->Stroke();
 ?> 
