69cd152e9452b19fdd4327f7a72d4c17dd5316d0
Andrew Miller <andrew.miller@gmail.com>
2012-04-13 13:49:31 -0400

    preparing to alter default text
    gathering all the default text together in one file

---
+++ statusnet/default-text.php
@@ -0,0 +1,140 @@
+<?php
+
+Welcome
+------------
+/* 
+StepStream will help you and your friends get and stay fit! 
+*/
+
+StepStream will help you and your friends get and stay fit!
+
+ * Wear your pedometer everywhere you go
+ * Enter your steps each day via SMS or web
+ * Share your activity with friends!
+ 
+ 
+Register
+------------
+/*
+With this form you can create a new account. You can then post notices and link up to friends and colleagues.
+
+[ ]  I understand that content and data of StepStream are private and confidential. My text and files remain under my own copyright. All rights reserved.
+
+[Email] Used only for updates, announcements, and password recovery.
+
+[Full Name] Longer name, preferably your "real" name.
+*/
+
+/*
+[Registration successful]
+
+Congratulations, andrewmillergt! And welcome to StepStream. From here, you may want to...
+
+More goes here later
+
+(Check your email and click on the link to confirm your email address.)
+*/
+
+Stream
+------------
+/*
+This is the public timeline for StepStream but no one has posted anything yet. Be the first to post!
+*/
+
+Tips
+------------
+/*
+This is the timeline for testuser1 and friends but no one has posted anything yet. Try subscribing to more people, join a group or post something yourself.
+*/
+
+Me
+-------------
+/*
+This is the timeline for testuser1, but testuser1 hasn't posted anything yet. Seen anything interesting recently? You haven't posted any notices yet, now would be a good time to start :)
+*/
+
+Settings
+-------------
+/*
+[Email preferences]
+Send me notices of new subscriptions through email.
+Send me email when someone adds my notice as a favorite.
+Send me email when someone sends me a private message.
+Send me email when someone sends me an "@-reply".
+Allow friends to nudge me and send me an email.
+Publish a MicroID for my email address.
+*/
+
+Emails
+-------------
+/*
+[Email address confirmation]
+Hey, andrewmatoxy.
+
+Someone just entered this email address on StepStream.
+
+If it was you, and you want to confirm your entry, use the URL below:
+
+       http://127.0.0.1/~andrew/StepStream/statusnet/index.php/main/confirmaddress/AN3TRWQJ67KXW
+
+If not, just ignore this message.
+
+Thanks for your time,
+StepStream
+*/
+
+/*
+[Roger C (@testuser1) added your notice as a favorite]
+Roger C (@testuser1) just added your notice from Monday, 09-Apr-12 15:20:23 UTC as one of their favorites.
+
+The URL of your notice is:
+
+http://127.0.0.1/~andrew/StepStream/statusnet/index.php/notice/9
+
+The text of your notice is:
+
+Hello!
+
+You can see the list of Roger C's favorites here:
+
+http://127.0.0.1/~andrew/StepStream/statusnet/index.php/testuser1/favorites
+
+Faithfully yours,
+StepStream.
+
+----
+Change your email address or notification options at http://127.0.0.1/~andrew/StepStream/statusnet/index.php/settings/email
+*/
+
+/*
+[Roger C (@testuser1) sent a notice to your attention]
+Roger C (testuser1) just sent a notice to your attention (an '@-reply') on StepStream.
+
+The notice is here:
+
+       http://127.0.0.1/~andrew/StepStream/statusnet/index.php/notice/10
+
+It reads:
+
+       Howdy
+
+The full conversation can be read here:
+
+       http://127.0.0.1/~andrew/StepStream/statusnet/index.php/conversation/6#notice-10
+
+You can reply back here:
+
+       http://127.0.0.1/~andrew/StepStream/statusnet/index.php/notice/new?replyto=testuser1&inreplyto=10
+
+The list of all @-replies for you here:
+
+http://127.0.0.1/~andrew/StepStream/statusnet/index.php/andrewmatoxy/replies
+
+Faithfully yours,
+StepStream.
+
+----
+Change your email address or notification options at http://127.0.0.1/~andrew/StepStream/statusnet/index.php/settings/email
+*/
+
+*/
