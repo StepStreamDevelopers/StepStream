9d21005fd2c336ca07f0718c00f08c56ef7fd015
Gayathri <gayathri6@gatech.edu>
2013-03-04 23:28:56 -0500

    REST API function added to get subscribed users

--- REST/server.php
+++ REST/server.php
@@ -7,11 +7,11 @@ class Server {
        to identify the resource.
     */
               
-    private $dbhost = 'host';
+    private $dbhost = 'mysql.steplab.org';
 
-    private $dbuser = 'user';
-    private $dbpass = 'pass';
-    private $dbname = 'dbname';
+    private $dbuser = 'steplab';
+    private $dbpass = 'stepstream8903';
+    private $dbname = 'steplab2013a';
     private $con;
     public function serve() {
         $this->con = mysql_connect($this->dbhost,$this->dbuser,$this->dbpass);
@@ -71,6 +71,8 @@ class Server {
               $this->display_scores($profileId);
             else if($valType == "tokens")
               $this->display_tokens($profileId);
+            else if($valType == "subscriptions")
+              $this->display_subscriptions($profileId);
             break;
 
         default:
@@ -127,6 +129,22 @@ class Server {
           {
                  echo $row['tokenSum'];
                 } 
+      }
+      
+      
+      
+       private function display_subscriptions($profileId) {
+        $dbquery = "SELECT * FROM  subscription WHERE subscriber =$profileId";
+        mysql_select_db($this->dbname, $this->con);
+
+        $result = mysql_query($dbquery);
+        $i = 0;
+        while($row = mysql_fetch_array($result))
+          {
+                 $profileIDList[$i++] = $row['subscribed'] ;
+                }
+                
+                print_r($profileIDList); 
       }
     
     private function paths($url) {
